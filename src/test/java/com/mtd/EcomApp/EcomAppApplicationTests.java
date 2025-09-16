9f2b188a8a8d8a520b5d7d0eebd9d9407e5c7669
hemuhr <hrhemanth29@gmail.com>
2025-09-16 16:36:24 +0530

    ecom

---
+++ src/test/java/com/mtd/EcomApp/EcomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtd.EcomApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
